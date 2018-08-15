f079052e224a88468a0e271b34166371492b7f2c
Pedro Felipe Gonçalves de Arruda <pedrofelipega@gmail.com>
2018-08-14 21:04:01 -0400

    Programa final

--- 2015_8/Compiladores/Tarefa1_13-08-18/programa03.py
+++ 2015_8/Compiladores/Tarefa1_13-08-18/programa03.py
@@ -2,12 +2,20 @@ arquivo = input('Digite o nome do arquivo:')
 file = open(arquivo, 'r')
 
 linha = 0
-caracteres = 0
+contador = 0
+car_total = 0
 
 for i in file:
 	linha = linha + 1
+	palavra = i.split(" ")
+	print(palavra)
+	contador = contador + len(palavra)
+	car_total = car_total + len(i)
 
+palavra = contador 
+letras = car_total - (palavra - 1) - (linha - 1)
+print('O arquivo {} tem: {} linha(s), {} palavra(s), {} letras e {} caracteres total'.format(arquivo, linha, palavra, letras, car_total))
 
-	
-print('O arquivo {} tem: {} linhas'.format(arquivo, linha))
+ashfbas = input(".")
+
 file.close
