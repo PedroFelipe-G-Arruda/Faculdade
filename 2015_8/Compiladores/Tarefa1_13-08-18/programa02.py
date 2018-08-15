fe2be9c2e5ceacf123b16b0c7bb50ad7791b37c2
Pedro Felipe Gonçalves de Arruda <pedrofelipega@gmail.com>
2018-08-14 21:03:34 -0400

    Programa final

--- 2015_8/Compiladores/Tarefa1_13-08-18/programa02.py
+++ 2015_8/Compiladores/Tarefa1_13-08-18/programa02.py
@@ -2,9 +2,7 @@ file = open('arquivo1.txt','r')
 manipulador = file.readlines()
 file.close
 manipulador.sort()
-x =len(manipulador)
 
 file = open('arquivo2.txt','w')
-for i in range(0,x):
-	file.write(manipulador[i])
+file.writelines(manipulador)
 file.close
