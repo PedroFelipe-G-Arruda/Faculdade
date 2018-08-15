baf725ea3d014e75864f82b1599161c64ed6cd5a
Pedro Felipe Gonçalves de Arruda <pedrofelipega@gmail.com>
2018-08-14 22:38:23 -0400

    Programa fucionando mas não finalizado

---
+++ 2015_8/Compiladores/Tarefa1_13-08-18/programa04.py
@@ -0,0 +1,21 @@
+file = open("voo.txt","r")
+
+origem = input("Digite a sua cidade de origem:")
+destino = input("Digiteo a cidade destino:") 
+
+flag = 0
+
+#tem que fazer a formatação
+
+for i in file:
+	#if origem and destino in i:
+		voo = i.split(",")
+		if (origem == voo[2]) and (destino == voo[3]):
+			print(i)
+			flag = 1
+
+
+if (flag == 0):
+	print("“NÃO EXISTEM VOOS")
+
+i = input('.') 
