bc00f0e6669a863b8584bf2ca744b0b428ce4c0c
Pedro Felipe Gonçalves de Arruda <pedrofelipega@gmail.com>
2018-10-04 00:36:29 -0400

    att

--- 2015_6/Compiladores/Lexico/A_Lexico.py
+++ 2015_6/Compiladores/Lexico/A_Lexico.py
@@ -1,6 +1,15 @@
 import sys
-# sao passados dois argumentos no maximo
 
+
+def transicao(estado, letra):
+    for line in tabela:
+        if estado + '|' + letra in line:
+            print(line)
+            return line.split('|')[2].rstrip()
+    return "error"
+
+
+# sao passados dois argumentos no maximo
 
 # pega os argumentos e coloca na lista parametros (param)
 param = sys.argv[1:]
@@ -16,17 +25,90 @@ nparam = len(param)
 if nparam == 2:
     if param[0] == "-lt":
         print("lista de tokens")
+        codigoFonte = param[1]
     else:
         print("\n{:-^40}\n".format("ERRO"))
-        print(f"Argumento {param[0]} nao reconhecido\n")
+        print("Argumento {} nao reconhecido\n".format(param[0]))
         print("-> Passar argumento corretamente <-\n")
         sys.exit()
-
-    teste = param[2].split(".")
-    print(teste)
-
-    print(param[nparam - 1])
-    print(nparam)
+else:
+    codigoFonte = param[0]
 
 
-#with open()
+# carrega da tabela de tokens
+with open('tabelaTokens.txt', 'r') as file:
+    tabela = file.readlines()
+    #print(tabela)
+
+with open(codigoFonte, 'r') as file:
+    codigo = file.readlines()
+    #print(codigo)
+
+estado_finais = {
+    'q5':'inicio',
+    'q8':'fim',
+    'q14':'escrever',
+    'q16':'condicao_se',
+    'q18':'tipo',
+    'q21':'ler',
+    'q25':'condicao_repeticao',
+    'q28':'fim_repeticao',
+    'q29':'virgula',
+    'q30':'fecha_parenteses',
+    'q31':'abre_parenteses',
+    'q32':'dois_pontos',
+    'q33':'final_linha',
+    'q34':'matematica_divisao',
+    'q35':'matematica_multiplicacao',
+    'q36':'matematica_subtracao',
+    'q37':'matematica_soma',
+    'q38':'matematica_igual',
+    'q39':'logica_igual',
+    'q40':'senao',
+    'q41':'ponto interrogacao',
+    'q44':'texto',
+    'q45':'numero',
+    'q46':'variavel',
+    'q47':'logica_maior',
+    'q48':'logica_maior_que',
+    'q49':'lociga_menor',
+    'q50':'logica_menor_que'
+    'q52':'logica_diferente'
+
+
+}
+
+nlinha = 0
+
+for linha in codigo:
+    estado_atual = estado_anterior = "q0"
+    nlinha +=1
+    ncoluna = 0
+    for i in linha:
+       # print("Estado atual: {}".format(estado_atual))
+        print("Letra: {}".format(i))
+        print("Estado atenrior: {}".format(estado_anterior))
+        estado_anterior = estado_atual
+        estado_atual = transicao(estado_atual, i)
+        ncoluna += 1
+        print("Estado atual: {}".format(estado_atual))
+
+        if estado_atual + '\n' in estados_finais:
+            print(linha)
+        '''
+        for l in estados_finais:
+            #print(l)
+            if estado_atual in l:
+                print(l)
+        '''
+    '''
+    print("\n\n")
+    print(estado_anterior)
+    print(estado_atual)
+    if  estado_atual == "error":
+        print(linha)
+    else:
+        print(estado_anterior)
+        print("Erro")
+        sys.exit()
+'''
