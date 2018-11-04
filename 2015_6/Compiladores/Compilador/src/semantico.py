f9ac66ae298da8783e79d387ae9ab3da92050bcb
Pedro Felipe Gonçalves de Arruda <pedrofelipega@gmail.com>
2018-11-03 23:21:08 -0400

    Analizador Semantico Finalizado

--- 2015_6/Compiladores/Compilador/src/semantico.py
+++ 2015_6/Compiladores/Compilador/src/semantico.py
@@ -1,31 +1,34 @@
 def analisador(listaTokens):
-
     def valor(linha):
-        lista = []
+        lista = ""
         while listaTokens[linha].split("|")[1] != "final_linha":
-            lista.append(listaTokens[linha].split("|")[0])
-            linha +=1
-        return lista
+            lista = lista + listaTokens[linha].split("|")[0] + " "
+            linha += 1
+        return lista.rstrip()
 
-    dicVariavel = {} # Dicionario com as variaveis e seus valores
-    listaErros = [] # Listade erros
+    dicVariavel = {}  # Dicionario com as variaveis e seus valores
+    listaErros = []  # Listade erros
     # Verificação de declaração de variaveis
-    cont = 0 # contador
-    for i in listaTokens:   # i percorre a lista de tokens
-        if i.split("|")[1] == "variavel":   # Verifica se i e variavel
-            print(f"Variavel: [{i.split('|')[0]}]")
-            if i.split("|")[0] in dicVariavel.keys():   # Verifica se a variavel ja esta no dicionario
-                print("ok")
+    cont = 0  # contador
+    for i in listaTokens:  # i percorre a lista de tokens
+        if i.split("|")[1] == "variavel":  # Verifica se i e variavel
+            if listaTokens[cont - 1].split('|')[0] == "int":
+                if not i.split('|')[0] in dicVariavel.keys():  # Verifica de a variavel esta sendo declarada
+                    dicVariavel.update({i.split('|')[0]: None})  # Adiciona a variavel no dicionario
+                else:
+                    print(f"Variavel: [{i.split('|')[0]}] ja declarada")  # Adiciona o erro na lista de erro
+                    break
 
-            elif listaTokens[cont - 1].split("|")[0] == "int":  # Verifica de a variavel esta sendo declarada
-                dicVariavel.update({i.split('|')[0]: None}) # Adiciona a variavel no dicionario
-                print("variavel add no dicionario")
+            elif i.split('|')[0] in dicVariavel.keys() and not i.split('|')[0] in listaErros:  # Verifica se a variavel ja esta no dicionario
+                if listaTokens[cont + 1].split("|")[1] == "atribuicao":
+                    dicVariavel.update({i.split('|')[0]: valor(cont + 2)})  # Adiciona a variavel no dicionario
 
-            else:   # Caso a variavel nao estaja no dicionario, e nao esteja sendo declarada
-                listaErros.append(f"Variavel: [{i.split('|')[0]}] nao declarada")   # Adiciona o erro na lista de erro
-                print("nao declarado")
-
+            else:  # Caso a variavel nao estaja no dicionario, e nao esteja sendo declarada
+                print(f"Variavel: {i.split('|')[0]} nao declarada")   # Adiciona o erro na lista de erro
+                break
         cont += 1
-    print(listaErros)
-    print(dicVariavel)
-    return ()
+    for i in listaTokens:  # i percorre a lista de tokens
+        if i.split("|")[0] == "/" and listaTokens[cont - 1].split('|')[0]:  # Verifica se i e variavel
+            print("divisao por zero")
+    print("\nAnalize Semantica Concluida!")
+    return "ok"
