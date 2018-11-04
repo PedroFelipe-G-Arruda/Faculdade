d779ec1902c8b0be9f31a639e90807943dec7f46
Pedro Felipe Gonçalves de Arruda <pedrofelipega@gmail.com>
2018-11-03 23:21:15 -0400

    Add files via upload

--- 2015_6/Compiladores/Compilador/test.m
+++ 2015_6/Compiladores/Compilador/test.m
@@ -1,16 +1,13 @@
 begin
 	int n;
-	int n;
 	int f <- 1;
+	int n;
 	output("Digite um numero para saber o fatorial dele");
 	input(n);
-	a <- (-1);
 	int b;
 	loop n >= 1:
 		f <- f * n;
 		n <- n - 1;
 	lend;
 	output("O fatorial e" f);
-	
-
 end
